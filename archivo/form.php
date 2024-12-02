36094101a2463bf4fdb63ff43c52cc12b04e8f7c
RackGamesDev <137448965+RackGamesDev@users.noreply.github.com>
2024-12-02 10:16:31 +0100

    Update form.php

--- archivo/form.php
+++ archivo/form.php
@@ -6,7 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    <!--formulario para enviar archivo, el enctype es para que se envien archivos, aqui habria que especificar el tipo, cantidad y tamagno de archivos-->
+    <!--Formulario para enviar archivo, el enctype es para que se envien archivos, aqui habria que especificar el tipo, cantidad y tamagno de archivos-->
     <form action="datosArchivo.php" method="post" enctype="multipart/form-data">
         <input type="file" name="archivo" size="20">
         <input type="submit" value="enviar archivo">
