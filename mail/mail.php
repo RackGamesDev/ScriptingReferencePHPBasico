80d122a50fbe90910217b17f456474a090cb28f0
RackGamesDev <137448965+RackGamesDev@users.noreply.github.com>
2024-12-02 10:13:30 +0100

    Update mail.php

--- mail/mail.php
+++ mail/mail.php
@@ -7,14 +7,14 @@
 </head>
 <body>
     <?php
-        #el host que interpreta el php debe tener un servidor de correo encendido
-        $texto = "aaa\r\nasdfaswdf";#texto del correo
-        $destinatario = "yesterlag1@gmail.com";#a quien va, no valida si el correo existe
-        $asunto = "afdasfdsaf";#asunto del correo
+        #El host que interpreta el php debe tener un servidor de correo encendido
+        $texto = "aaa\r\nasdfaswdf";#Texto del correo
+        $destinatario = "yesterlag1@gmail.com";#A quien va, no valida si el correo existe
+        $asunto = "afdasfdsaf";#Asunto del correo
         $headers="MIME-Version: 1.0\r\n";#headers varios no obligatorios
         $headers.="Content-type: text/html; charset=iso-8859-1\r\n";
-        $headers.="From: usuario que envia < prueba@asdfg.es >\r\n";#quien lo envia, obviamente es falso
-        $exito = mail($destinatario, $asunto, $texto, $headers);#enviarlo
+        $headers.="From: usuario que envia < prueba@asdfg.es >\r\n";#Quien lo envia, obviamente es falso
+        $exito = mail($destinatario, $asunto, $texto, $headers);#Enviarlo
         if(!$exito){
             echo "error";
         } else {
