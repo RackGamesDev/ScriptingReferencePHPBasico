01f82d2eb190edaaacbdd8aef1509d53b59311ac
RackGamesDev <137448965+RackGamesDev@users.noreply.github.com>
2024-12-02 10:16:14 +0100

    Retocar

--- archivo/datosArchivo.php
+++ archivo/datosArchivo.php
@@ -8,22 +8,22 @@
 <body>
     <h1>archivo recibido</h1>
 <?php
-    $nombre_archivo = $_FILES["archivo"]["name"];#variable que devuelve archivos, se especifica su nombre en el form y la propiedad a sacar
+    $nombre_archivo = $_FILES["archivo"]["name"];#Variable que devuelve archivos, se especifica su nombre en el form y la propiedad a sacar
     $tipo_archivo = $_FILES["archivo"]["type"];
     $tamagno_archivo = $_FILES["archivo"]["size"];
-    $carpeta_destino = $_SERVER["DOCUMENT_ROOT"] . "/phpbasico/archivo/archivos/";#especificar la carpeta de destino apartir del htdocs
+    $carpeta_destino = $_SERVER["DOCUMENT_ROOT"] . "/phpbasico/archivo/archivos/";#Especificar la carpeta de destino apartir del htdocs
 
-    move_uploaded_file($_FILES["archivo"]["tmp_name"], $carpeta_destino . $nombre_archivo);#guardarlo en la carpeta, conviene ponerle un nombre unequivoco (ej: timestamp)
+    move_uploaded_file($_FILES["archivo"]["tmp_name"], $carpeta_destino . $nombre_archivo);#Guardarlo en la carpeta, conviene ponerle un nombre unequivoco (ej: timestamp)
     
 
-    $conexion = mysqli_connect("localhost", "root", "");#ahora se guardara el archivo en una base de datos, con el campo blob (longblob)
+    $conexion = mysqli_connect("localhost", "root", "");#Ahora se guardara el archivo en una base de datos, con el campo blob (longblob)
     mysqli_select_db($conexion, "base");
     mysqli_set_charset($conexion, "utf8");
-    $archivo_objetivo = fopen($carpeta_destino . $nombre_archivo, "r");#abre un archivo local, la r es de read ya que hay de varios tipos (mirar documentacion)
-    $contenido = fread($archivo_objetivo, $tamagno_archivo);#el contenido del archivo ya esta volcado en esta variable
+    $archivo_objetivo = fopen($carpeta_destino . $nombre_archivo, "r");#Abre un archivo local, la r es de read ya que hay de varios tipos (mirar documentacion)
+    $contenido = fread($archivo_objetivo, $tamagno_archivo);#El contenido del archivo ya esta volcado en esta variable
     $contenido = addslashes($contenido);
-    fclose($archivo_objetivo);#cerrar el archivo
-    $consulta="INSERT INTO archivos (nombre, tipo, contenido) VALUES ('$nombre_archivo', '$tipo_archivo', '$contenido')";#consulta para insertar el archivo (ya no hace falta tenerlo en local)
+    fclose($archivo_objetivo);#Cerrar el archivo
+    $consulta="INSERT INTO archivos (nombre, tipo, contenido) VALUES ('$nombre_archivo', '$tipo_archivo', '$contenido')";#Consulta para insertar el archivo (ya no hace falta tenerlo en local)
     $resultado = mysqli_query($conexion, $consulta);
     if(mysqli_affected_rows($conexion) > 0){
         echo "guardado";
@@ -31,7 +31,7 @@
         echo "error";
     }
 
-    echo "<img src='data:image/png; base64," . base64_encode($contenido) . "'>";#en caso de que el archivo sea una imagen se representa asi, para otros tipos de archivo hay que cambiar lo de data:image/png al tipo de archivo
+    echo "<img src='data:image/png; base64," . base64_encode($contenido) . "'>";#En caso de que el archivo sea una imagen se representa asi, para otros tipos de archivo hay que cambiar lo de data:image/png al tipo de archivo
 ?>
 </body>
 </html>
